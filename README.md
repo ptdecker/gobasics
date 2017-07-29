37d10b6cd452f635e8d53f0196853da7a2ffc95c
P. Todd Decker <ptdecker@mac.com>
2017-07-29 10:15:42 -0600

    Solution to exercise 3-10

--- README.md
+++ README.md
@@ -80,6 +80,8 @@ Exercises postfixed with a "(+)" indicate ones for which a benchmark test has be
   * mandelbrot6: Re-implements 'mandelbrot1' using big.Rat with benchmarks
 * 3-09 -
   * mandelbrot7: Re-implements 'mandelbrot1' as a web service supporting x,y coordinates and zoom parameters. For example, try 'http://localhost:8000/?zoom=1000&x=-0.7325&y=-0.241'.
+* 3-10 -
+  * comma1: A non-recursive version of 'comma'
 
 
 ### Extras
---
+++ src/ch03/comma1/main.go
@@ -0,0 +1,52 @@
+// A non-recursive version of comma that leverages bytes.Buffer instead of string concatination
+
+package main
+
+import (
+	"fmt"
+	"bytes"
+	"unicode/utf8"
+	"unicode"
+)
+
+// reverse()
+// c.f. https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go#10030772
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+// comma()
+
+func comma(s string) string {
+	var buf bytes.Buffer
+	i := 1
+	for len(s) > 0 {
+		r, size := utf8.DecodeLastRuneInString(s)
+		s = s[:len(s)-size]
+		if unicode.IsDigit(r) {
+			buf.WriteRune(r)
+			if (len(s) > 0 && i%3 == 0) {
+				buf.WriteString(",")
+			}
+			i++
+		}
+	}
+	return reverse(buf.String())
+}
+
+// main()
+
+func main() {
+	fmt.Println(comma("123456789"))
+	fmt.Println(comma(""))
+	fmt.Println(comma("ABCD"))
+	fmt.Println(comma("ABC1234DEF"))
+	fmt.Println(comma("1234.56"))
+	fmt.Println(comma("1234.5678"))
+	fmt.Println(comma("1234 5678"))
+}
